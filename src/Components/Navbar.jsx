339c26f0396f4076af338cf6de3b82e416dddfe5
Noah-1711 <noahnirmal11@gmail.com>
2023-03-16 21:42:25 +0530

    react-redux-001

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,27 +1,38 @@
-import React, { useState } from 'react'
+import React, { useContext } from 'react'
 import './Navbar.css'
-import { toggledarktheme } from '../Redux/action'
-import {useDispatch} from 'react-redux'
+import { Mycontext } from "../App";
+import { Link } from 'react-router-dom';
+
+const darkheader = { color: "#5ddbf7", backgroundColor: "#0f172a" };
+const lightheader = { color: "#0f172a", backgroundColor: "#5ddbf7" };
+const light = { color: "black", backgroundColor: "white", width: "100%" };
+const darklinks = { color: "white"};
+const lightlinks = { color: "black"};
+
 
 export const Navbar = () => {
-    const dispatch= useDispatch()
-    const data=[{
-        hello:"heello"
-    }]
+    const context = useContext(Mycontext);
+
   return (
-    <div className='navbar'>
-        <h1>Geekster</h1>
-        <div className="navbarcontent">
-            <a href=""> home</a>
-            <a href="">contact</a>
-            <a href="">service</a>
+    <div className='navbarcontainer'>
+        <div className="alerttheme" style={context.theme ? light : light}>
+        <p>
+        I have changed theme to{" "}
+        <span>{context.theme ? "Dark !!!" : "Light !!!"}</span>
+      </p>
         </div>
-        <button onClick={()=>{
-            dispatch(toggledarktheme(data))
-            console.log("hello")
-       
-            
-        }}>toggel dark</button>
-    </div>
+  
+    <div className="navbar" style={context.theme ? darkheader : lightheader}>
+        <div className="navheading">
+          <h2>Geekster</h2>
+        </div>
+        <div className="navlinks">
+         
+          <Link className='navlink-style' to="/" style={context.theme ? darklinks : lightlinks} >Home</Link>
+          <Link className='navlink-style' to="/about" style={context.theme ? darklinks : lightlinks}>About</Link>
+          <Link  className='navlink-style'to="/services" style={context.theme ? darklinks : lightlinks}>Services</Link>
+        </div>
+      </div>
+      </div>
   )
 }
